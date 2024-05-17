dd1be7da8a62f8964de574873d8a27698896745f
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-05-17 12:14:48 +0200

    feat: add lens owner address to multi protocol example

--- templates/next-starter-with-examples/app/examples/new-api-multi-protocol/frames/who-am-i/route.tsx
+++ templates/next-starter-with-examples/app/examples/new-api-multi-protocol/frames/who-am-i/route.tsx
@@ -1,13 +1,27 @@
+import { LensClient, production } from "@lens-protocol/client";
 import { frames } from "../frames";
 
 export const POST = frames(async (ctx) => {
+  let walletAddress: string | undefined =
+    // farcaster
+    ctx.message?.requesterCustodyAddress ||
+    // xmtp
+    ctx.message?.verifiedWalletAddress;
+
+  if (ctx.clientProtocol?.id === "lens") {
+    const lensClient = new LensClient({ environment: production });
+
+    const profile = await lensClient.profile.fetch({
+      forProfileId: ctx.message?.profileId,
+    });
+
+    walletAddress = profile?.ownedBy.address;
+  }
+
   return {
     image: (
       <div tw="flex p-5">
-        You are{" "}
-        {ctx.message?.requesterCustodyAddress ||
-          ctx.message?.verifiedWalletAddress}{" "}
-        from {ctx.clientProtocol?.id}
+        You are {walletAddress} from {ctx.clientProtocol?.id}
       </div>
     ),
   };
